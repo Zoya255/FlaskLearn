751b5086028aae6a76b83782beaf0f5c29bb52b8
Cet <korlyakov.2001@mail.ru>
2021-03-10 00:53:43 +1000

    update study project

--- projects/microblog/app/routes.py
+++ projects/microblog/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -39,4 +40,52 @@ def index():
 
 @app.route('/info')
 def info():
-	return "<h1>Тут нет ничего интересного. Пока.</h1>"
+	info = {
+		'title': 'Стартовая страница'
+	}
+	user = {
+		'name': 'Халва'
+	}
+
+	data = f"Тут есть важная инфа: {app.config['SECRET_KEY']}"
+
+	return render_template("info.html", info = info, user = user, data = data)
+
+
+@app.route('/user/<int:user_id>/')
+def user(user_id):
+	info = {
+		'title': 'Стартовая страница'
+	}
+	user = {
+		'name': 'Халва'
+	}
+
+	data = f"Hello, {user_id}"
+
+	return render_template("info.html", info = info, user = user, data = data)
+
+
+@app.route('/test')
+def requestdata():
+	info = {
+		'title': 'Стартовая страница'
+	}
+	user = {
+		'name': 'Халва'
+	}
+
+	return render_template( "test.html", info = info, user = user, request = request )
+
+
+@app.route('/login')
+def login():
+	info = {
+		'title': 'Вход в систему'
+	}
+	user = {
+		'name': 'Халва'
+	}
+	form = LoginForm()
+
+	return render_template( "login.html", info = info, user = user, form = form )
